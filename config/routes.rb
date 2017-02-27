e2bbe3c8b970792a947e5c6b095334407da5414e
Jon Harder <jonharder6@gmail.com>
2017-02-26 20:07:45 -0600

    go straight to the pantry

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :ingredients
   resources :pantry_entries
   resources :units
-  root 'ingredients#index'
+  root 'pantry_entries#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
